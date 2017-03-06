f813e6e6fe7f7634ad1daa73dc956f0ffca38750
Juthawong Naisanguansee <juthawong@users.noreply.github.com>
2017-03-06 08:05:55 +0700

    Update YourCordova.bat

--- YourCordova.bat
+++ YourCordova.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p input="Enter Your Path To Android SDK"
+set /p input="Enter Your Path To Android SDK  "
 set ANDROID_HOME=%input%
 set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
 echo Finish Set Path at %input% . Thanks
